7374cbd72d412e24dd769286092a6b277a53b23a
s17321 <127015609+s17321@users.noreply.github.com>
2024-04-25 19:30:46 +0200

    feature/modify_kedro_data (#10)

--- ml-pipeline/src/ml_pipeline/pipelines/data_processing/nodes.py
+++ ml-pipeline/src/ml_pipeline/pipelines/data_processing/nodes.py
@@ -17,52 +17,52 @@ def _parse_money(x: pd.Series) -> pd.Series:
     return x
 
 
-def preprocess_companies(companies: pd.DataFrame) -> pd.DataFrame:
-    """Preprocesses the data for companies.
+# def preprocess_companies(companies: pd.DataFrame) -> pd.DataFrame:
+#     """Preprocesses the data for companies.
 
-    Args:
-        companies: Raw data.
-    Returns:
-        Preprocessed data, with `company_rating` converted to a float and
-        `iata_approved` converted to boolean.
-    """
-    companies["iata_approved"] = _is_true(companies["iata_approved"])
-    companies["company_rating"] = _parse_percentage(companies["company_rating"])
-    return companies
-
-
-def preprocess_shuttles(shuttles: pd.DataFrame) -> pd.DataFrame:
-    """Preprocesses the data for shuttles.
-
-    Args:
-        shuttles: Raw data.
-    Returns:
-        Preprocessed data, with `price` converted to a float and `d_check_complete`,
-        `moon_clearance_complete` converted to boolean.
-    """
-    shuttles["d_check_complete"] = _is_true(shuttles["d_check_complete"])
-    shuttles["moon_clearance_complete"] = _is_true(shuttles["moon_clearance_complete"])
-    shuttles["price"] = _parse_money(shuttles["price"])
-    return shuttles
+#     Args:
+#         companies: Raw data.
+#     Returns:
+#         Preprocessed data, with `company_rating` converted to a float and
+#         `iata_approved` converted to boolean.
+#     """
+#     companies["iata_approved"] = _is_true(companies["iata_approved"])
+#     companies["company_rating"] = _parse_percentage(companies["company_rating"])
+#     return companies
 
 
-def create_model_input_table(
-    shuttles: pd.DataFrame, companies: pd.DataFrame, reviews: pd.DataFrame
-) -> pd.DataFrame:
-    """Combines all data to create a model input table.
+# def preprocess_shuttles(shuttles: pd.DataFrame) -> pd.DataFrame:
+#     """Preprocesses the data for shuttles.
 
-    Args:
-        shuttles: Preprocessed data for shuttles.
-        companies: Preprocessed data for companies.
-        reviews: Raw data for reviews.
-    Returns:
-        Model input table.
+#     Args:
+#         shuttles: Raw data.
+#     Returns:
+#         Preprocessed data, with `price` converted to a float and `d_check_complete`,
+#         `moon_clearance_complete` converted to boolean.
+#     """
+#     shuttles["d_check_complete"] = _is_true(shuttles["d_check_complete"])
+#     shuttles["moon_clearance_complete"] = _is_true(shuttles["moon_clearance_complete"])
+#     shuttles["price"] = _parse_money(shuttles["price"])
+#     return shuttles
 
-    """
-    rated_shuttles = shuttles.merge(reviews, left_on="id", right_on="shuttle_id")
-    rated_shuttles = rated_shuttles.drop("id", axis=1)
-    model_input_table = rated_shuttles.merge(
-        companies, left_on="company_id", right_on="id"
-    )
-    model_input_table = model_input_table.dropna()
-    return model_input_table
+
+# def create_model_input_table(
+#     shuttles: pd.DataFrame, companies: pd.DataFrame, reviews: pd.DataFrame
+# ) -> pd.DataFrame:
+#     """Combines all data to create a model input table.
+
+#     Args:
+#         shuttles: Preprocessed data for shuttles.
+#         companies: Preprocessed data for companies.
+#         reviews: Raw data for reviews.
+#     Returns:
+#         Model input table.
+
+#     """
+#     rated_shuttles = shuttles.merge(reviews, left_on="id", right_on="shuttle_id")
+#     rated_shuttles = rated_shuttles.drop("id", axis=1)
+#     model_input_table = rated_shuttles.merge(
+#         companies, left_on="company_id", right_on="id"
+#     )
+#     model_input_table = model_input_table.dropna()
+#     return model_input_table
--- ml-pipeline/src/ml_pipeline/pipelines/data_processing/pipeline.py
+++ ml-pipeline/src/ml_pipeline/pipelines/data_processing/pipeline.py
@@ -8,21 +8,22 @@ def create_pipeline(**kwargs) -> Pipeline:
         [
             node(
                 func=preprocess_companies,
-                inputs="companies",
-                outputs="preprocessed_companies",
-                name="preprocess_companies_node",
-            ),
-            node(
-                func=preprocess_shuttles,
-                inputs="shuttles",
-                outputs="preprocessed_shuttles",
-                name="preprocess_shuttles_node",
-            ),
-            node(
-                func=create_model_input_table,
-                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
-                outputs="model_input_table",
-                name="create_model_input_table_node",
-            ),
+                inputs="aug_train",
+                outputs="preprocessed_aug_train",
+                name="preprocess_aug_train_node",
+            )
+            # ,
+            # node(
+            #     func=preprocess_shuttles,
+            #     inputs="shuttles",
+            #     outputs="preprocessed_shuttles",
+            #     name="preprocess_shuttles_node",
+            # ),
+            # node(
+            #     func=create_model_input_table,
+            #     inputs=["preprocessed_shuttles", "preprocessed_aug_train", "reviews"],
+            #     outputs="model_input_table",
+            #     name="create_model_input_table_node",
+            # ),
         ]
     )
