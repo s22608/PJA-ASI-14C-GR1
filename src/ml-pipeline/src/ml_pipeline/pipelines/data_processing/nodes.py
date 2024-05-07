45e5493568af827e0886d95009b4746c60ac612e
damson99 <damian.wieczorek99@gmail.com>
2024-05-07 21:07:26 +0200

    creating input table

--- src/ml-pipeline/src/ml_pipeline/pipelines/data_processing/nodes.py
+++ src/ml-pipeline/src/ml_pipeline/pipelines/data_processing/nodes.py
@@ -21,23 +21,5 @@ def preprocess_aug_train(data: pd.DataFrame) -> pd.DataFrame:
     return data
 
 
-# def create_model_input_table(
-#     shuttles: pd.DataFrame, companies: pd.DataFrame, reviews: pd.DataFrame
-# ) -> pd.DataFrame:
-#     """Combines all data to create a model input table.
-
-#     Args:
-#         shuttles: Preprocessed data for shuttles.
-#         companies: Preprocessed data for companies.
-#         reviews: Raw data for reviews.
-#     Returns:
-#         Model input table.
-
-#     """
-#     rated_shuttles = shuttles.merge(reviews, left_on="id", right_on="shuttle_id")
-#     rated_shuttles = rated_shuttles.drop("id", axis=1)
-#     model_input_table = rated_shuttles.merge(
-#         companies, left_on="company_id", right_on="id"
-#     )
-#     model_input_table = model_input_table.dropna()
-#     return model_input_table
+def create_model_input_table(aug_test: pd.DataFrame) -> pd.DataFrame:
+    return aug_test
